5a36d89d603604290fa5bbade9cdf7d6ffbfa3db
Luis Enrique Ch <melu16.sipi@gmail.com>
2026-08-02 18:29:31 -0500

    Correccion login y eliminacion imagen antigua

--- js/login.js
+++ js/login.js
@@ -1,11 +1,14 @@
-const correo = document.getElementById("correo").value;
-const clave = document.getElementById("clave").value;
+function iniciarSesion() {
 
-if (
-    correo === "u26277929@utp.edu.pe" &&
-    clave === "Lindes2204"
-) {
-    window.location = "inicio.html";
-} else {
-    alert("Correo o contraseña incorrectos.");
+    const correo = document.getElementById("correo").value;
+    const clave = document.getElementById("clave").value;
+
+    if (
+        correo === "u26277929@utp.edu.pe" &&
+        clave === "123456"
+    ) {
+        window.location.href = "inicio.html";
+    } else {
+        alert("Correo o contraseña incorrectos.");
+    }
 }
